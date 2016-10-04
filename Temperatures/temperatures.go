f6f419ba8fc98b78f7d2da88189b3e3bdfaccbb4
David Albert <david@bronto.com>
2016-10-04 16:41:42 -0400

    finished Temperatures

--- Temperatures/temperatures.go
+++ Temperatures/temperatures.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "os"
 import "bufio"
+import "strconv"
 
 //import "strings"
 //import "strconv"
@@ -15,15 +16,28 @@ import "bufio"
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Buffer(make([]byte, 1000000), 1000000)
+	scanner.Split(bufio.ScanWords)
 
 	// n: the number of temperatures to analyse
 	var n int
 	scanner.Scan()
 	fmt.Sscan(scanner.Text(), &n)
-
-	scanner.Scan()
-	//temps := scanner.Text() // the n temperatures expressed as integers ranging from -273 to 5526
+	fmt.Fprintln(os.Stderr, n)
+	var m, md, td int = 0, 5527, 0
+	for scanner.Scan() {
+		if t, err := strconv.Atoi(scanner.Text()); err == nil {
+			if t < 0 {
+				td = t * -1
+			} else {
+				td = t
+			}
+			if td < md || (td == md && t > 0) {
+				m = t
+				md = td
+			}
+		}
+	}
 
 	// fmt.Fprintln(os.Stderr, "Debug messages...")
-	fmt.Println("result") // Write answer to stdout
+	fmt.Println(m) // Write answer to stdout
 }
